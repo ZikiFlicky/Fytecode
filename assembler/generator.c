89e9b896edde0bb83b76858cb6070d353ae427dc
Yuval <yuval.tasher@gmail.com>
2022-04-18 00:04:50 +0300

    Remove useless comments

--- assembler/generator.c
+++ assembler/generator.c
@@ -17,23 +17,14 @@ void Fy_Generator_makeSpace(Fy_Generator *generator, size_t length) {
     }
 }
 
-// void Fy_Generator_addCString(Fy_Generator *generator, uint8_t *cstr) {
-//     size_t len = strlen((char*)cstr); // FIXME: Should this be strlen? we need to work with uint8_t
-//     Fy_Generator_makeSpace(generator, len);
-//     memcpy(&generator->output[generator->idx], cstr, len * sizeof(uint8_t));
-//     generator->idx += len;
-// }
-
 void Fy_Generator_addByte(Fy_Generator *generator, uint8_t b) {
     Fy_Generator_makeSpace(generator, 1);
-    // printf("%zu: %x\n", generator->idx, b);
     generator->output[generator->idx++] = b;
 }
 
 void Fy_Generator_addConst16(Fy_Generator *generator, uint16_t w) {
     Fy_Generator_makeSpace(generator, 2);
     // Little endianness
-    // printf("%zu: %x\n", generator->idx, w);
     generator->output[generator->idx] = w & 0xff;
     generator->output[generator->idx + 1] = w >> 8;
     generator->idx += 2;

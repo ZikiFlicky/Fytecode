87a3132b45899062e9f5a047cc04c36d303b8a78
Yuval <yuval.tasher@gmail.com>
2022-04-21 18:33:05 +0300

    Make fib example use procedures

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,8 +1,17 @@
 start:
+    mov dx 10 ; amount of rounds
+    call fib
+    end
+
+proc fib
+    push ax
+    push bx
+    push cx
+    push dx
+
     mov ax 0
     mov bx 1
-    mov dx 10 ; amount of rounds
-fib:
+fib_loop:
     cmp dx 0
     je end_fib
 
@@ -12,6 +21,13 @@ fib:
     mov bx cx
 
     sub dx 1
-    jmp fib
+    jmp fib_loop
 end_fib:
     debug
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp fib

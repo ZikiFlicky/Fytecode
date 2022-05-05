be2f8a4670e985bd55836c5ccf52d786f3fe03bb
Yuval <yuval.tasher@gmail.com>
2022-05-05 18:23:56 +0300

    Use macros for variables

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,13 +1,16 @@
 CODE
 start:
     push 11
-    push 0
     push 1
+    push 0
     call fib
     debug
     end
 
 ; returns into ax
+a = bp + 4
+b = bp + 6
+n = bp + 8
 proc fib
     push bp
     mov bp sp
@@ -15,24 +18,25 @@ proc fib
     push cx
     push dx
 
-    mov ax [bp + 8]
+    mov ax [n]
     cmp ax 0
     jg calculate
-    mov ax [bp + 6]
+    mov ax [a]
     jmp return
 calculate:
-    mov ax [bp + 6] ; load a
-    mov bx [bp + 4] ; load b
+    mov ax [a] ; load a
+    mov bx [b] ; load b
     ; create next
     mov cx ax
     add cx bx
 
-    mov dx [bp + 8]
+    mov dx [n]
     sub dx 1
     push dx
-    push bx
     push cx
+    push bx
     call fib
+
 return:
     pop dx
     pop cx
